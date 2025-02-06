f4a2690e66eb5d4e2e96724e2d910c0fbe0dfad5
davidperezboza <david.perez@babelgroup.com>
2025-02-06 13:59:26 +0100

    Video 351

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,48 @@
-import { NavBar } from "../"
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours, format, parse, startOfWeek, getDay,  } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+
+import { NavBar } from "../";
+
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const eventos = [{
+  title: 'Cumpleaños de jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    id: '123',
+    name: 'Fernando',
+  },
+}];
 
 export const CalendarPage = () => {
   return (
     <>
       <NavBar />
+
+      <Calendar
+        localizer={localizer}
+        events={eventos}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80ox)' }}
+      />
     </>
   )
 }
