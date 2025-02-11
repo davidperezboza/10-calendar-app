97dde0e7444b66cb5d9118b44191bac63eedeab1
davidperezboza <david.perez@babelgroup.com>
2025-02-11 15:34:01 +0100

    Video 366

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { onAddNewEvent, onSetActiveEvent } from '../store/';
+import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from '../store/';
 
 export const useCalendarStore = () => {
     const {events, activeEvent} = useSelector(state => state.calendar)
@@ -12,6 +12,7 @@ export const useCalendarStore = () => {
     const startSavingEvent = async(calendarEvent) => {
         if(calendarEvent._id){
             //Actualizando
+            dispatch(onUpdateEvent({...calendarEvent}));
         }else{
             //Creando
             dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime()}));
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -32,7 +32,15 @@ export const calendarSlice = createSlice({
       state.events.push(payload);
       state.activeEvent = null;
     },
+
+    onUpdateEvent: (state, {payload}) => {
+      state.events = state.events.map(event => {
+        if(event._id === payload._id) return payload;
+        return event;
+      })
+      state.activeEvent = null;
+    },
   },
 })
 
-export const {onSetActiveEvent, onAddNewEvent} = calendarSlice.actions;
+export const {onSetActiveEvent, onAddNewEvent, onUpdateEvent} = calendarSlice.actions;
